8458b8cdbf34ef87f1001ad44191635888f13ccc
gmap-nitro <geraldo.filho92@hotmail.com>
2024-01-28 08:19:58 -0300

    fix: add env new api

--- AluraImersao_vanilla/src/index.js
+++ AluraImersao_vanilla/src/index.js
@@ -2,10 +2,8 @@ const searchInput = document.getElementById('search-input')
 const resultsArtist = document.querySelector('#result-artist')
 const playlistContainer = document.getElementById('result-playlists')
 
-const APIVALOR = Netlify.env.get('REACT_APP_API')
-
 function requestApi(searchTerm) {
-  fetch(`${APIVALOR}/artists?name_like=${searchTerm}`)
+  fetch(`${REACT_APP_API}/artists?name_like=${searchTerm}`)
     .then((res) => res.json())
     .then((result) => displayResults(result))
 }
